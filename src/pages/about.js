fd596736c606a348460a609e8506a76e8084f32f
Sara Alkhamri <s.alkhamri@gmail.com>
2020-08-12 19:17:53 -0700

    added headerText prop to header component in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header />
+      <Header headerText="About Gatsby"/>
       <p>Such wow. Very React.</p>
     </div>
   )
